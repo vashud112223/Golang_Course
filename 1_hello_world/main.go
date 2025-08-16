7e05f23274fb8e4612e1274c14912cfd40208890
Tanya Verma <ashutosh.btech.ec19@iiitranchi.ac.in>
2025-08-16 20:34:32 +0530

    01_Hello World

--- 1_hello_world/main.go
+++ 1_hello_world/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main(){
-	fmt.Println("hello world")
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
 }
+
+// build the code
+// go build .\1_hello_world\main.go
+// .\main.exe -> then execute this
+//  go run .\1_hello_world\main.go -> another way of running the code
