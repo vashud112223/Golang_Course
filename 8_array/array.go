d4cdbe552f239c15b2177b10d6ed0ffb824961fe
Tanya Verma <ashutosh.btech.ec19@iiitranchi.ac.in>
2025-08-17 11:00:30 +0530

    08_arrays

---
+++ 8_array/array.go
@@ -0,0 +1,25 @@
+package main
+
+// array -> numbered sequence of specific length
+func main() {
+	// int-> 0, bool-> false, string-> ""
+	// var nums [4]int
+
+	// fmt.Println(len(nums))
+	// nums[0] = 5
+	// fmt.Println(nums[0])
+	// var nums [4]string
+
+	// fmt.Println(nums)
+
+	// to declare it in single line
+	// nums := [4]int{1, 2, 3}
+	// fmt.Println(nums)
+
+	// 2d array
+	// nums := [2][2]int{{3, 4}, {5, 6}}
+	// fmt.Println(nums)
+
+	// fixed size , that is predictable, Memory optimization, constant time access
+
+}
