d9955f2c335edb6258f5b81a348be78cd952913f
Tanya Verma <ashutosh.btech.ec19@iiitranchi.ac.in>
2025-08-19 21:06:24 +0530

    16_structs

---
+++ 16_structs/struct.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type order struct {
+	id          string
+	amount      float32
+	status      string
+	createdTime time.Time
+}
+
+// constructor
+func newOrder(id string, amount float32, status string) *order {
+	//initial setup
+	myOrder := order{
+		id:     id,
+		amount: amount,
+		status: status,
+	}
+	return &myOrder
+}
+
+// created methods
+// receiver type
+func (o *order) changeStatus(status string) {
+	o.status = status // no need to dereference it, struct will take care of it
+}
+
+func (o order) getAmount() float32 {
+	return o.amount // when we are returning , no need to give pointer
+}
+func main() {
+	// instantiate
+	// myOrder := order{
+	// 	id:     "1",
+	// 	amount: 30.4,
+	// 	status: "pending",
+	// }
+	myOrder := newOrder("1", 50, "accepted")
+
+	myOrder2 := order{
+		id:     "2",
+		amount: 50.4,
+		status: "received",
+	}
+
+	language := struct {
+		name   string
+		isGood bool
+	}{"golang", true}
+
+	fmt.Println(language)
+
+	// myOrder.createdTime = time.Now()
+	// myOrder.changeStatus("paid")
+	fmt.Println(myOrder)
+	fmt.Printf("%+v\n", *myOrder)
+	fmt.Println(myOrder2)
+	// fmt.Println(myOrder.getAmount())
+}
