c902ead5813102bc314c55dde61adfcd1e233be9
Tanya Verma <ashutosh.btech.ec19@iiitranchi.ac.in>
2025-08-20 13:24:39 +0530

    18_enum

---
+++ 18_enums/enum.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type OrderStatus int
+
+const (
+	Received OrderStatus = iota
+	Confirmed
+	Prepared
+	Delivered
+)
+
+func changeStatus(status OrderStatus) {
+	fmt.Println("message status is", status)
+}
+
+func main() {
+	changeStatus(Confirmed)
+}
